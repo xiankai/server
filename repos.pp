ede8f1a54713bcd5326c4e6318b3ac793acd8851
KJ <xiankai@gmail.com>
2013-09-15 16:07:44 +0800

    repos

---
+++ repos.pp
@@ -0,0 +1,22 @@
+############
+#Setup repos
+############
+yumrepo { "nginx":
+	baseurl => "http://nginx.org/packages/centos/6/$basearch/",
+	gpgcheck => 0,
+	enabled => 1
+}
+
+yumrepo { "epel":
+	baseurl => "http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm",
+	gpgcheck => 0,
+	enabled => 1
+}
+
+exec { "vcsrepo":
+	command => "sudo puppet module install puppetlabs/vcsrepo"
+}
+
+exec { "vcsrepo":
+	command => "sudo puppet module install puppetlabs/mysql"
+}
