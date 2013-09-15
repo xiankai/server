da932ec5fa45f81a61b4522e898e7f2cfea63f19
KJ <xiankai@gmail.com>
2013-09-15 23:32:49 +0800

    Tested the repos this time

--- repos.pp
+++ repos.pp
@@ -2,16 +2,16 @@
 #Setup repos
 ############
 yumrepo { "nginx":
-	name => "Official Nginx Packages",
-	baseurl => "http://nginx.org/packages/centos/6/$basearch/",
+	name => "nginx_repo",
+	baseurl => "http://nginx.org/packages/centos/6/\$basearch/",
 	gpgcheck => 0,
 	enabled => 1
 }
 
 yumrepo { "epel":
-	name => "Extra Packages for Enterprise Linux 6 - $basearch",
-	baseurl => "http://download.fedoraproject.org/pub/epel/6/$basearch",
-	mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=epel-6&amp;arch=$basearch",
+	name => "epel6_repo",
+	baseurl => "http://download.fedoraproject.org/pub/epel/6/\$basearch",
+	mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=epel-6&amp;arch=\$basearch",
 	gpgcheck => 0,
 	enabled => 1
 }
