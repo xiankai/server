a6de0f7e0c5fc63a8897fb023c0f46597c4fc1f1
kj <xiankai@gmail.com>
2013-10-12 19:07:22 +0630

    Add php 5.4 repo and fixed bug "name not found" when installing via yum

--- repos.pp
+++ repos.pp
@@ -2,16 +2,27 @@
 #Setup repos
 ############
 yumrepo { "nginx":
-	name => "nginx_repo",
+	name => "nginx",
+	descr	=> "nginx",
 	baseurl => "http://nginx.org/packages/centos/6/\$basearch/",
 	gpgcheck => 0,
 	enabled => 1
 }
 
 yumrepo { "epel":
-	name => "epel6_repo",
+	name => "epel6",
+	descr	=> "epel6",
 	baseurl => "http://download.fedoraproject.org/pub/epel/6/\$basearch",
 	mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=epel-6&amp;arch=\$basearch",
+	gpgcheck => 0,
+	enabled => 1
+}
+
+yumrepo { "webtatic":
+	name => "webtatic",
+	descr	=> "webtatic",
+	baseurl => "http://repo.webtatic.com/yum/el6/$basearch/",
+	mirrorlist	=> "http://mirror.webtatic.com/yum/el6/$basearch/mirrorlist",
 	gpgcheck => 0,
 	enabled => 1
 }
